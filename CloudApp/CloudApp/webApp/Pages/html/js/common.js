250204a37f1c1f7e0fb63140a06ffbdf466a10a6
TODO <317543501@qq.com>
2019-06-03 07:26:28 +0800

    插入排序

--- CloudApp/CloudApp/webApp/Pages/html/js/common.js
+++ CloudApp/CloudApp/webApp/Pages/html/js/common.js
@@ -1,20 +1,20 @@
 ﻿/**
  * 插入排序
- * @param {any} ary
+ * @param {any} ary  需要排序的数组
  */
 
 function insert(ary) {
-    var handAry = [];//存储的是手里已经抓取的牌 
-    handAry.push(ary[0]); //先抓一张牌（一般是转第一张）
+    var handAry = [];//存储的是手里已经抓取的牌  规定：左小右大的顺序
+    handAry.push(ary[0]); //先抓一张牌（一般是抓第一张）
     //循环抓取后面的牌
     for (var i = 1; i < ary. length; i++) {
         var cur = ary[i];//本次新抓的这张牌
-        for (var j = handAry.length - 1; j >= 0; j--) {
-            if (cur > handAry[j]) {   // 新抓的牌与手里的牌作比较                 handAry[j]： 当前手里抓的牌
+        for (var j = handAry.length - 1; j >= 0; j--) {   // 新抓的牌与手里的牌作比较   从右往左比
+            if (cur > handAry[j]) {   // handAry[j]： 当前手里抓的牌
                 handAry.splice(j + 1, 0, cur);
                 break;
             }
-            if (j === 0) {
+            if (j === 0) { //本次新抓的这张牌 比手里最后一张牌都小 
                 handAry.unshift(cur);
             }
 
@@ -22,5 +22,5 @@ function insert(ary) {
     }
     return handAry;
 }
-var ary1 = [2, 3, 6, 1, 31, 2, 43, 5, 122, 21, 55, 88];
-console.log(insert(ary1));
+
+
