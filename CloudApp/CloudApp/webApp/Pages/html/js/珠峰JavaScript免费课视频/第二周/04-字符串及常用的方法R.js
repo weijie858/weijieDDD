c922a227ae283920d0bd12cac74130958e74344a
TODO <317543501@qq.com>
2019-06-11 18:26:17 +0800

    tjiao

---
+++ CloudApp/CloudApp/webApp/Pages/html/js/珠峰JavaScript免费课视频/第二周/04-字符串及常用的方法R.js
@@ -0,0 +1,14 @@
+﻿var str = 'wdsfsjfdjsflsjfljdsljlfjsl dsfjlsjflsjf dsflajlfdjslfjds';
+console.log(String.prototype);
+console.log(str.length);
+//substr()
+//substring()
+//slice()
+//split()
+//indexOf()
+//lastIndexOf()
+//replace()
+var st = 'zhufengsdssd2018zhufeng';
+var dd=st.replace('zhufeng', '珠峰');
+console.log(st);
+console.log(dd);
