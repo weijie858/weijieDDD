1377e3d00f82c8b6433b8638c6b6fc5527b7cf80
TODO <317543501@qq.com>
2019-06-11 18:37:34 +0800

    vs2015

---
+++ CloudApp/CloudApp/WebApplication1/html/3.js
@@ -0,0 +1,15 @@
+Array.prototype.myUnique = function myUnique() {
+    var obj = {};
+    for (var i = 0; i < this.length; i++) {
+        var item = this[i];
+        if (typeof obj[item] !== 'undefined') {
+            this[i] = this[this.length - 1];
+            this.length--;
+            i--;
+            continue;
+        }
+        obj[item] = item;
+    }
+    obj = null;
+    return this;
+}
